62570994cd91a624f8d8604a744eb3c848ff8242
Jeremy Clark <shiblisa0@gmail.com>
2020-09-26 14:44:44 -0700

    Removed fixtures from test_board.py
    They are now just locally-defined variables inside
    their respective tests.

--- tests/test_board_checker.py
+++ tests/test_board_checker.py
@@ -2,42 +2,8 @@ from decorate_all_methods import decorate_all_methods
 import pytest
 from catan.core.board_checker import BoardChecker
 
-def test_value_handler(value_handler_data):
-    for elem in value_handler_data['assert_raises']:
-        with pytest.raises(AssertionError):
-            BoardChecker.value_handler(elem[0], elem[1])
-
-    for elem in value_handler_data['pass']:
-        BoardChecker.value_handler(elem[0], elem[1])
-
-def test_is_coord(is_coord_data):
-    for elem in is_coord_data['assert_raises']:
-        with pytest.raises(AssertionError):
-            BoardChecker.is_coord(elem)
-
-    for elem in is_coord_data['pass']:
-        BoardChecker.is_coord(elem)
-
-def test_is_edge_coord(is_edge_coord_data):
-    for elem in is_edge_coord_data['assert_raises']:
-        with pytest.raises(AssertionError):
-            BoardChecker.is_edge_coord(elem)
-
-    for elem in is_edge_coord_data['pass']:
-        BoardChecker.is_edge_coord(elem)
-
-def test_is_intersection_coord(is_intersection_coord_data):
-    for elem in is_intersection_coord_data['assert_raises']:
-        with pytest.raises(AssertionError):
-            BoardChecker.is_intersection_coord(elem)
-
-    for elem in is_intersection_coord_data['pass']:
-        BoardChecker.is_intersection_coord(elem)
-
-@decorate_all_methods(pytest.fixture(scope="module"))
-class _fixtures:
-    def value_handler_data():
-        data = {
+def test_value_handler():
+    value_handler_data = {
                 "assert_raises": (
                     (1, (2,3,4,5)),
                     (5, (3, 6, 5.5, 'r')),
@@ -51,10 +17,15 @@ class _fixtures:
                     ([1,2,3], [[1,2,3], (), 'foo', 'cat'])
                 )
             }
-        return data
+    for elem in value_handler_data['assert_raises']:
+        with pytest.raises(AssertionError):
+            BoardChecker.value_handler(elem[0], elem[1])
 
-    def is_coord_data():
-        data = {
+    for elem in value_handler_data['pass']:
+        BoardChecker.value_handler(elem[0], elem[1])
+
+def test_is_coord():
+    is_coord_data = {
                 "assert_raises": (
                     [1,2],
                     {'foo': 8, 'bar': 2},
@@ -69,10 +40,15 @@ class _fixtures:
                     (1,2)
                 )
             }
-        return data
+    for elem in is_coord_data['assert_raises']:
+        with pytest.raises(AssertionError):
+            BoardChecker.is_coord(elem)
 
-    def is_edge_coord_data():
-        data = {
+    for elem in is_coord_data['pass']:
+        BoardChecker.is_coord(elem)
+
+def test_is_edge_coord():
+    is_edge_coord_data = {
                 "assert_raises": (
                     [1,2],
                     {'foo': 8, 'bar': 2},
@@ -88,10 +64,15 @@ class _fixtures:
                     (87.5,5)
                 )
             }
-        return data
+    for elem in is_edge_coord_data['assert_raises']:
+        with pytest.raises(AssertionError):
+            BoardChecker.is_edge_coord(elem)
 
-    def is_intersection_coord_data():
-        data = {
+    for elem in is_edge_coord_data['pass']:
+        BoardChecker.is_edge_coord(elem)
+
+def test_is_intersection_coord():
+    is_intersection_coord_data = {
                 "assert_raises": (
                     [1,2],
                     {'foo': 8, 'bar': 2},
@@ -106,4 +87,10 @@ class _fixtures:
                     (32283,3)
                 )
             }
-        return data
+    for elem in is_intersection_coord_data['assert_raises']:
+        with pytest.raises(AssertionError):
+            BoardChecker.is_intersection_coord(elem)
+
+    for elem in is_intersection_coord_data['pass']:
+        BoardChecker.is_intersection_coord(elem)
+
