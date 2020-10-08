9e7cd9bbee2b4b1ea08799d88f7e1ab3cac25d91
Jeremy Clark <shiblisa0@gmail.com>
2020-10-08 12:06:37 -0700

    Add spaces test_player.py
    Adhering to PEP suggestions.

--- catan/player.py
+++ catan/player.py
@@ -18,58 +18,100 @@ class Player:
         self.board = board
         self.name = name
 
-    def longest_road_path(self) -> list:
-        all_paths = []
+    # Helper recursion function
+    def _exhaust_pathways(
+            current_intersection,
+            visited=[],
+            prev_matches = set()
+    ):
+        """
+        Helper recursion function for longest_road_path. Global in class
+        to make it testable. Given a starting coordinate, _exhaust_pathways
+        will iterate through all of the roads that are connected to it. It
+        does this by finding the edges that are connected to it and determining
+        whether a road which this player owns is on that edge. If it is, then
+        the algorithm will find the intersection that the road leads to. If the
+        intersection is not in the set of intersection that the last iteration
+        visited, and the intersection has not been visited before, the it will
+        add this intersection to its list of matches. If the algorithm finds
+        no matches, this means that it has reached the end of a road.
+        Algorithm does not need an intersection_plot because the plot
+        can be reconstructed with just one intersection. Every intersection
+        stores its surroundings, which store their surroundings, which
+        store their surroundings, etc. However, the algorithm does not
+        need to reconstruct the entire board. At any given time, it only
+        needs information about its past iterations and the current
+        intersection. Therefore, it can be called with just a single
+        intersection at the beginning, since it has no stack.
+        """
 
-        # Helper recursion function
-        def exhaust_pathways(current_coord, coord_set, visited=[], prev_matches=set()):
-            local_visited = copy.copy(visited)
-            local_visited.append(current_coord)
+        local_visited = copy.copy(visited)
+        local_visited.append(current_intersection)
 
-            x, y = current_coord[0], current_coord[1]
-            possible_matches = {(x+1,y), (x-1,y), (x,y+1), (x,y-1)}
+        matches = set()
 
-            matches = set()
-            for elem in possible_matches:
-                if elem in coord_set:
-                    if elem not in local_visited:
-                        if elem not in prev_matches:
-                            matches.add(elem)
+        for edge in current_intersection.surroundings['edges']:
+            if edge.occupant is self:
+                connecting_intersection = None
+                for intersection in edge.intersections:
+                    if intersection is not current_intersection:
+                        connecting_intersection = intersection
+                if connecting_intersection not in visited:
+                    if connecting_intersection not in prev_matches:
+                        matches.add(connecting_intersection)
 
-            if not matches:
-                all_paths.append(local_visited)
-            else:
-                for match in matches:
-                    exhaust_pathways(match, coord_set, local_visited, matches)
+        if not matches:
+            yield local_visited
+        else:
+            for match in matches:
+                _exhaust_pathways(match, local_visited, matches)
 
+    def all_paths(self) -> list:
+        """
+        Returns a list of all of the ordered lists
+        that _exhaust_pathways would return for every
+        intersection with only one road connected to
+        it. Starting a cycle of recursion with two or
+        more roads attached to an intersection would
+        be redundant. It would not find the length
+        of a road from beginning to end, but from
+        the center to one end instead.
+        """
         # Collection of the coordinates intersections
         # which have one or more of this player's roads connected to them
-        intersection_coord_set = set()
+        intersection_set = set()
         for intersection in self.board.intersection_plot.values():
             for edge in intersection.surroundings['edges']:
                 if edge.occupant is self:
-                    intersection_coord_set.add(intersection.coords)
+                    intersection_set.add(intersection)
 
+        all_paths = []
+        for intersection in instersection_set:
+            for item in list(self._exhaust_pathways(intersection)):
+                all_paths.append(item)
 
-        for coord in intersection_coord_set:
-            exhaust_pathways(coord, intersection_coord_set)
+        return all_paths
 
-        # Makes a dict where keys are the lengths of their
-        # corresponding paths
-        path_lens = {len(path):path for path in all_paths}
+    def longest_road_path(self):
+        all_paths = self.all_paths()
+        for path in all_paths:
+            visited_intersections = set()
+            visited_roads = set()
+            for intersection in path:
+                  
 
-        # Returns the path with the highest length
-        return path_lens[max(path_lens.keys())]
 
     def longest_road_len(self) -> int:
-        # If the road is a loop, the length will be the
-        # length of the intersection list returned by
-        # self.longest_road_path(). If the length of
-        # the pathway is 2, then the the length will
-        # be one. If it has a distict end and beginning
-        # and has a length greater than 2, it will return
-        # one less than the length of the intersection list
-        # returned by self.longest_road_path().
+        """
+        If the road is a loop, the length will be the
+        length of the intersection list returned by
+        self.longest_road_path(). If the length of
+        the pathway is 2, then the the length will
+        be one. If it has a distict end and beginning
+        and has a length greater than 2, it will return
+        one less than the length of the intersection list
+        returned by self.longest_road_path().
+        """
 
         pathway = self.longest_road_path()
 
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,5 +1,4 @@
-# from .context.catan.core.player import Player, Road
-from .context import catan
+from context import catan
 from catan.board_components import Road
 from catan.player import Player
 
@@ -10,25 +9,25 @@ import pytest
     [
         (
             [(0, 2.5), (0.5, 3), (1, 3.5), (1.5, 4),
-            (2.5, 4), (3, 3.5), (3.5, 3), (4, 2.5),
-            (3.5, 4), (4.5, 4), (5.5, 1), (6.5, 1),
-            (9, 3.5)],
+             (2.5, 4), (3, 3.5), (3.5, 3), (4, 2.5),
+             (3.5, 4), (4.5, 4), (5.5, 1), (6.5, 1),
+             (9, 3.5)],
             8
         ),
         (
-            [(0,2.5)],
+            [(0, 2.5)],
             1
         ),
         (
-            [(3,3.5), (3.5,3), (4.5,3), (5,3.5),
-             (3.5,4), (4.5,4)],
+            [(3, 3.5), (3.5, 3), (4.5, 3), (5, 3.5),
+             (3.5, 4), (4.5, 4)],
             6
         ),
         (
-            [(3.5,2), (3.5,3), (3.5,4), (3.5,5),
-             (2.5,2), (2.5,3), (2.5,4), (2.5,5),
-             (2,4.5), (2,2.5), (4,4.5), (4,2.5),
-             (3,3.5), (3,1.5), (2.5,1)],
+            [(3.5, 2), (3.5, 3), (3.5, 4), (3.5, 5),
+             (2.5, 2), (2.5, 3), (2.5, 4), (2.5, 5),
+             (2, 4.5), (2, 2.5), (4, 4.5), (4, 2.5),
+             (3, 3.5), (3, 1.5), (2.5, 1)],
             14
         )
     ]
@@ -43,4 +42,5 @@ def test_longest_road(starter_board_data, road_list, max_len):
         edge.road = Road(local_player)
         edge.occupant = local_player
     assert local_player.longest_road_len() == max_len, \
-        f'local_player.longest_road should return {max_len} but returns {local_player.longest_road_len()} instead'
+        'local_player.longest_road should return {max_len} but returns ' \
+        f'{local_player.longest_road_len()} instead'
